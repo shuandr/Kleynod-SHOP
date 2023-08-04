f92b750d4b25931e28ece746300aea9cafb7eae3
shuandr <shuandr@gmail.com>
2023-08-04 22:46:44 +0300

    ver. 1.7

--- assets/js_plugins/myAngular.js
+++ assets/js_plugins/myAngular.js
@@ -203,7 +203,7 @@ app.controller('kleynodShopCtrl', function($scope, $http, $route, $routeParams, 
         var data = getFormData(); // get the values submitted in the form
 
         // var url = event.target.action; //
-        var url = "https://script.google.com/macros/s/AKfycby1MXFzklbdY3b_6nPIGOpJaTaNdr8iOY4bEH8jpyYMID-O1DJJzTeN-Kn3nVG7vBwx7w/exec"; //
+        var url = "https://script.google.com/macros/s/AKfycbz2V1g6XGBGYVqN4FjMAn_07dyiqYfRikA1MerVuO2iVvaibeDNNCLZDH9akKOzA60wUA/exec"; //
         var xhr = new XMLHttpRequest();
         xhr.open('POST', url);
         // xhr.withCredentials = true;
